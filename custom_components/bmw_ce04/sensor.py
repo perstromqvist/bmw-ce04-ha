660bf99ef0b093c322232023ddbc35afd309e372
perstromqvist <123633834+perstromqvist@users.noreply.github.com>
2026-05-29 12:58:57 +0200

    Update sensor.py

--- custom_components/bmw_ce04/sensor.py
+++ custom_components/bmw_ce04/sensor.py
@@ -22,56 +22,15 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .entity import CE04Entity
 from .models import CE04Data
-from .helpers import map_color
 
-
-# ---------------------------------------------------------
-# Sensor description with value extractor
-# ---------------------------------------------------------
 
 @dataclass(frozen=True, kw_only=True)
 class CE04SensorDescription(SensorEntityDescription):
     value_fn: Callable[[CE04Data], object]
 
 
-# ---------------------------------------------------------
-# Main “bike info” sensor (with entity_picture)
-# ---------------------------------------------------------
-
-class CE04BikeInfoSensor(CE04Entity, SensorEntity):
-    """Main CE04 sensor that provides entity picture."""
-
-    _attr_name = "CE04"
-    _attr_icon = "mdi:motorbike"
-
-    def __init__(self, coordinator, bike_id: str):
-        super().__init__(coordinator, bike_id)
-        self._attr_unique_id = f"{self.bike_slug}_info"
-        self._attr_suggested_object_id = f"{self.bike_slug}_info"
-
-    @property
-    def native_value(self):
-        """Return a simple status string."""
-        if not self.bike:
-            return None
-        return "online"
-
-    @property
-    def entity_picture(self) -> str | None:
-        """Return dynamic image based on bike color."""
-        if not self.bike:
-            return None
-
-        color = map_color(self.bike.color)
-        return f"/local/{color}.jpg"
-
-
-# ---------------------------------------------------------
-# Regular sensors
-# ---------------------------------------------------------
-
 SENSORS: tuple[CE04SensorDescription, ...] = (
-    # ---- Battery -----------------------------------------------------
+    # ---- EV battery --------------------------------------------------
     CE04SensorDescription(
         key="battery_level",
         translation_key="battery_level",
@@ -83,7 +42,6 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         suggested_display_precision=0,
         value_fn=lambda bike: bike.battery_level,
     ),
-
     CE04SensorDescription(
         key="remaining_range",
         translation_key="remaining_range",
@@ -95,8 +53,7 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         suggested_display_precision=0,
         value_fn=lambda bike: bike.remaining_range_electric_km,
     ),
-
-    # ---- Odometer / Trip ---------------------------------------------
+    # ---- Odometer / trip ---------------------------------------------
     CE04SensorDescription(
         key="total_mileage",
         translation_key="total_mileage",
@@ -108,7 +65,6 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         suggested_display_precision=0,
         value_fn=lambda bike: bike.total_mileage_km,
     ),
-
     CE04SensorDescription(
         key="trip1",
         translation_key="trip1",
@@ -116,10 +72,10 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         icon="mdi:road-variant",
         device_class=SensorDeviceClass.DISTANCE,
         native_unit_of_measurement=UnitOfLength.KILOMETERS,
+        state_class=SensorStateClass.TOTAL,
         suggested_display_precision=0,
         value_fn=lambda bike: bike.trip1_km,
     ),
-
     CE04SensorDescription(
         key="trip2",
         translation_key="trip2",
@@ -127,34 +83,40 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         icon="mdi:road-variant",
         device_class=SensorDeviceClass.DISTANCE,
         native_unit_of_measurement=UnitOfLength.KILOMETERS,
+        state_class=SensorStateClass.TOTAL,
         suggested_display_precision=0,
         value_fn=lambda bike: bike.trip2_km,
     ),
-
-    # ---- Tire pressures ----------------------------------------------
+    # ---- Tyre pressures ----------------------------------------------
     CE04SensorDescription(
         key="tire_pressure_front",
         translation_key="tire_pressure_front",
         name="Tire pressure front",
-        device_class=SensorDeviceClass.PRESSURE,
+        icon="mdi:gauge",
         native_unit_of_measurement=UnitOfPressure.BAR,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
         value_fn=lambda bike: bike.tire_pressure_front_bar,
     ),
-
     CE04SensorDescription(
         key="tire_pressure_rear",
         translation_key="tire_pressure_rear",
         name="Tire pressure rear",
-        device_class=SensorDeviceClass.PRESSURE,
+        icon="mdi:gauge",
         native_unit_of_measurement=UnitOfPressure.BAR,
         state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=1,
         value_fn=lambda bike: bike.tire_pressure_rear_bar,
     ),
-
-    # ---- Service & Diagnostics ---------------------------------------
+    # ---- Service & Diagnostics ----------------------------------------
+    CE04SensorDescription(
+        key="vin",
+        translation_key="vin",
+        name="VIN",
+        icon="mdi:barcode",
+        entity_category=EntityCategory.DIAGNOSTIC,
+        value_fn=lambda bike: bike.vin,
+    ),
     CE04SensorDescription(
         key="next_service_due_date",
         translation_key="next_service_due_date",
@@ -164,7 +126,6 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=lambda bike: bike.next_service_due_date,
     ),
-
     CE04SensorDescription(
         key="next_service_remaining_distance",
         translation_key="next_service_remaining_distance",
@@ -177,8 +138,7 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         suggested_display_precision=0,
         value_fn=lambda bike: bike.next_service_remaining_distance_km,
     ),
-
-    # ---- Connectivity -------------------------------------------------
+    # ---- Connectivity (diagnostic) -----------------------------------
     CE04SensorDescription(
         key="last_connected_time",
         translation_key="last_connected_time",
@@ -188,7 +148,6 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=lambda bike: bike.last_connected_time,
     ),
-
     CE04SensorDescription(
         key="last_activated_time",
         translation_key="last_activated_time",
@@ -198,7 +157,6 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         entity_category=EntityCategory.DIAGNOSTIC,
         value_fn=lambda bike: bike.last_activated_time,
     ),
-
     CE04SensorDescription(
         key="charging_time_estimation",
         translation_key="charging_time_estimation",
@@ -211,7 +169,6 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
         suggested_display_precision=0,
         value_fn=lambda bike: bike.charging_time_estimation_electric,
     ),
-
     CE04SensorDescription(
         key="battery_soh",
         translation_key="battery_soh",
@@ -225,16 +182,17 @@ SENSORS: tuple[CE04SensorDescription, ...] = (
 )
 
 
-# ---------------------------------------------------------
-# Generic sensor class
-# ---------------------------------------------------------
-
 class CE04Sensor(CE04Entity, SensorEntity):
     """Representation of a BMW CE 04 sensor."""
 
     entity_description: CE04SensorDescription
 
-    def __init__(self, coordinator, bike_id: str, description: CE04SensorDescription):
+    def __init__(
+        self,
+        coordinator,
+        bike_id: str,
+        description: CE04SensorDescription,
+    ) -> None:
         super().__init__(coordinator, bike_id)
         self.entity_description = description
         self._attr_unique_id = f"{self.bike_slug}_{description.key}"
@@ -247,10 +205,21 @@ class CE04Sensor(CE04Entity, SensorEntity):
             return None
         return self.entity_description.value_fn(self.bike)
 
+    @property
+    def entity_picture(self) -> str | None:
+        """Dynamisk bild baserat på hojens färg."""
+        if not self.bike or not self.bike.color:
+            return "/local/white.png"
 
-# ---------------------------------------------------------
-# Setup
-# ---------------------------------------------------------
+        raw_color = str(self.bike.color).upper()
+        color_map = {
+            "P0N3H": "white",
+            "P0NB5": "blue",
+            "P0N2M": "silver",
+        }
+        image_name = color_map.get(raw_color, "white")
+        return f"/local/{image_name}.png"
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -260,14 +229,10 @@ async def async_setup_entry(
     """Set up BMW CE 04 sensor entities."""
     coordinator = entry.runtime_data.coordinator
 
-    entities: list[SensorEntity] = []
-
-    for bike_id in coordinator.data:
-        # Add main bike info sensor (with image)
-        entities.append(CE04BikeInfoSensor(coordinator, bike_id))
-
-        # Add all regular sensors
-        for description in SENSORS:
-            entities.append(CE04Sensor(coordinator, bike_id, description))
+    entities: list[SensorEntity] = [
+        CE04Sensor(coordinator, bike_id, description)
+        for bike_id in coordinator.data
+        for description in SENSORS
+    ]
 
     async_add_entities(entities)
